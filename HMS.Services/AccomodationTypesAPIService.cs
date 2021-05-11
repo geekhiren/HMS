c8455a881a8144c00fa7a286079c8c2088d93b0c
Hiren Shekhda <shekhdah@gmail.com>
2021-05-11 13:17:10 +0530

    Compllet Project

---
+++ HMS.Services/AccomodationTypesAPIService.cs
@@ -0,0 +1,28 @@
+﻿using HMS.Data;
+using HMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Services
+{
+    public class AccomodationTypesAPIService
+    {
+        public IEnumerable<AccomodationType> GetAllATA()
+        {
+            var context = new HMSContext();
+
+            return context.AccomodationTypes;
+        }
+
+        public AccomodationType GetATAByID(int ID)
+        {
+            var context = new HMSContext();
+
+            return context.AccomodationTypes.Find(ID);
+        }
+    }
+
+}
---
+++ HMS/Code/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Code
+{
+    public class Variables
+    {
+        public const string HotelFullName = "International Resort";
+        public const string HotelShortName = "HFORH";
+        public const string HotelNameAbbreviate = "HMS";
+    }
+}
